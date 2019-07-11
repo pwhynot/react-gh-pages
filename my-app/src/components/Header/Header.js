d777e982d4f5f787b769ad8c57f03c1375a6908c
unknown <pwhynotfilms@gmail.com>
2019-07-11 15:44:12 -0500

    Header and navbar edited

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import "./style.css"
+
 const style = {
     HeaderStyle: {
         marginTop: 0,
@@ -9,9 +10,9 @@ const style = {
     }
 }
 function Header() {
-    return (<div style={style.HeaderStyle} className='headerDiv'>
+    return (<div style={style.HeaderStyle} className="headerDiv">
         <div><h2>Ghostbusters Clicky Game!</h2></div>
-        <h2 id="instructions">Click an image to gain points, click the same image twice game over!.</h2>
+        <h2 id="instructions">Click an image to gain points, click the same image twice game over!</h2>
     </div >
     );
 }
--- my-app/src/components/Navbar/navbar.js
+++ my-app/src/components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const score = 0;
 const highScore = 0;
